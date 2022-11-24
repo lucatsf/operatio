4b32bc37a7f897361a8d178cbdd225e7439ab609
Lucas Torres <lucastorresfellicio@gmail.com>
2022-11-23 23:37:25 -0300

    fix: collectCoverage is boolean

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverage: ['<rootDir>/src/**/*.ts'],
+  collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   testEnvironment: 'node',
